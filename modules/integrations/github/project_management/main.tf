1a5b2a65c576b3f0029723b0295720e95b9ac492
Marc <marc@schopper.rocks>
2025-07-09 10:46:53 +0200

    Move variable definitions for milestones, labels, and issues to a separate variables.tf file for better organization and clarity.

--- modules/integrations/github/project_management/main.tf
+++ modules/integrations/github/project_management/main.tf
@@ -12,32 +12,6 @@ locals {
   github_owner    = "marcbey"
 }
 
-variable "milestones" {
-  type = map(object({
-    title       = string
-    due_date    = string
-    description = string
-  }))
-  description = "Milestones, consider them the biggest deliverable unit."
-}
-
-variable "labels" {
-  type = map(object({
-    name  = string
-    color = string
-  }))
-  description = "The labels to tag the issues."
-}
-
-variable "issues" {
-  type = list(object({
-    title     = string
-    body      = string
-    labels    = list(string)
-    milestone = string
-  }))
-}
-
 # Configure the GitHub Provider
 provider "github" {
   owner = local.github_owner
