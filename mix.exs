a4539e62134dc910e13a01924af0658f9262584b
Marc <marc@schopper.rocks>
2025-07-14 13:14:31 +0200

    Add Dialyzer configuration and caching to CI/CD workflow

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule Kanban.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_core_path: "priv/plts/core.plt",
+        plt_file: {:no_warn, "priv/plts/project.plt"},
+        plt_add_apps: [:ex_unit]
+      ]
     ]
   end
 
@@ -54,7 +59,8 @@ defmodule Kanban.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
