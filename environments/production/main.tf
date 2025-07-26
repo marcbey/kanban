5d62c1a4cb222c0d3e8a53b5f55e8d70fb3cd644
Marc <marc@schopper.rocks>
2025-07-26 09:06:38 +0200

    Add EC2 instance and Security Group imports in Terraform configuration for Docker Swarm

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,3 +1,15 @@
 module "swarm" {
   source = "../../modules/cloud/aws/compute/swarm"
 }
+
+# Import the EC2 instance
+import {
+  to = module.swarm.aws_instance.docker-swarm-manager
+  id = "i-085909aee7aba67e1"
+}
+
+# Import the Security Group
+import {
+  to = module.swarm.aws_security_group.docker-swarm-sg
+  id = "sg-01eb2769e0afccbbd"
+}
