756bb9b9eadd35b5be5235635a6b2aa3e16e9d19
Marc <marc@schopper.rocks>
2025-07-30 13:34:25 +0200

    Update CI/CD workflow to use Ubuntu 22.04 and add GitHub branch protection configuration in Terraform. Introduce variables for repository settings and required status checks.

---
+++ modules/integrations/github/contributing_workflow/variables.tf
@@ -0,0 +1,15 @@
+variable "repository" {
+  description = "Name of the GitHub repository."
+  type        = string
+}
+
+variable "github_owner" {
+  description = "Owner/organization where the GitHub repository resides."
+  type        = string
+}
+
+variable "status_checks" {
+  description = "List of required status checks that must pass."
+  type        = list(string)
+  default     = []
+}
