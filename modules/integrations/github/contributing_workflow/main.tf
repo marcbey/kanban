4a281e46ff15bfa773e176cca5a0ea4aaf420e76
Marc <marc@schopper.rocks>
2025-08-01 23:12:13 +0200

    Disable branch protection temporarily

--- modules/integrations/github/contributing_workflow/main.tf
+++ modules/integrations/github/contributing_workflow/main.tf
@@ -11,16 +11,16 @@ provider "github" {
   owner = var.github_owner
 }
 
-resource "github_branch_protection" "main" {
-  repository_id  = var.repository
-  pattern        = "main"
-  enforce_admins = true
-  required_status_checks {
-    strict   = true
-    contexts = var.status_checks
-  }
-  required_pull_request_reviews {
-    require_code_owner_reviews      = false
-    required_approving_review_count = 1
-  }
-}
+# resource "github_branch_protection" "main" {
+#   repository_id  = var.repository
+#   pattern        = "main"
+#   enforce_admins = true
+#   required_status_checks {
+#     strict   = true
+#     contexts = var.status_checks
+#   }
+#   required_pull_request_reviews {
+#     require_code_owner_reviews      = false
+#     required_approving_review_count = 1
+#   }
+# }
